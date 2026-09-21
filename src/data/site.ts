dd8e3c2511d386ac94d9ea9748ea81e88be5f124
Alberto Moya <128317733+albertomoya-dev@users.noreply.github.com>
2026-09-21 19:51:33 +0200

    fix: enlazar la ficha de empresa de Google Maps en lugar de la dirección suelta

--- src/data/site.ts
+++ src/data/site.ts
@@ -19,7 +19,7 @@ export const site = {
 
   city: 'Sevilla',
   address: 'C.Mijail Gorbachov, 8, LOC 1, 41702 Dos Hermanas',
-  mapUrl: 'https://www.google.com/maps/place/C.+Mijail+Gorbachov,+8,+41702+Dos+Hermanas,+Sevilla/@37.285662,-5.9113787,20.5z/data=!4m6!3m5!1s0xd127064e44f666f:0x787742fc5a6c826e!8m2!3d37.2856754!4d-5.9110482!16s%2Fg%2F11c2c7wjjw?entry=ttu&g_ep=EgoyMDI2MDgwNC4wIKXMDSoASAFQAw%3D%3D',
+  mapUrl: 'https://maps.app.goo.gl/5E1CCUhbMqQSgJ6Y7',
   phone,
   phoneHref,
   email: getEnv('PUBLIC_CONTACT_EMAIL', 'PENDIENTE_EMAIL'),
